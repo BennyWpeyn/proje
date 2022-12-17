0ccece141d8595f24f283d47080e805edffa7c44
Zeyn28 <93488191+Zeyn28@users.noreply.github.com>
2022-12-17 13:43:18 +0300

    Supervisora 2 tane addStudent fonksiyonu eklendi

--- Main.java
+++ Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Main{
+
     public static void main(String[] args){
+        
+        Scanner sc2=new Scanner(System.in);
+
         UndergraduateS ogr1 = new UndergraduateS(1, "osman gültekin", 2011);
         System.out.println(ogr1.id_number);
+
+
+Supervisor sv=new Supervisor(125,"Nilsu Aksu");
+
+    sv.addUnderStudent();
+    sv.addGraduateStudent();
+
+
+
+
+
+
 
 
 
--- UndergraduateS.java
+++ UndergraduateS.java
@@ -1,8 +1,15 @@
-public class UndergraduateS extends Student {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class UndergraduateS extends Student {
+ Scanner sc=new Scanner(System.in);
 
     public UndergraduateS(int id_number, String name, int year) {
         super(id_number, name, year);
     }
+
+   
     
+   
 }
