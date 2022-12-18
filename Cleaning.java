aa59a8a27425feb0c6df78a563eb3f5488709d6c
Melike Atlı <90909913+melikeatli@users.noreply.github.com>
2022-12-18 17:42:15 +0300

    cleaning

---
+++ Cleaning.java
@@ -0,0 +1,12 @@
+public class Cleaning extends Staff {
+   
+
+    public Cleaning(int id_number,String name_surname, String task_info,int working_hours,int year_of_experience){
+        super(id_number,name_surname,task_info,working_hours,year_of_experience);
+        
+    }
+
+   
+
+
+}
