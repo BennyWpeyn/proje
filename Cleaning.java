c87dbf19c05b4505f270eadd830dda3d8435eddf
Melike Atlı <90909913+melikeatli@users.noreply.github.com>
2022-12-18 17:44:05 +0300

    deneme

--- Cleaning.java
+++ Cleaning.java
@@ -1,8 +1,16 @@
 public class Cleaning extends Staff {
    
 
-    public Cleaning(int id_number,String name_surname, String task_info,int working_hours,int year_of_experience){
-        super(id_number,name_surname,task_info,working_hours,year_of_experience);
+    public Cleaning(int id_number, String name_surname, String task_info, int working_hours, int year_of_experience,
+            int salary) {
+        super(id_number, name_surname, task_info, working_hours, year_of_experience, salary);
+    }
+
+    
+
+    @Override
+    public void net_salary(int salary, int year_of_experience) {
+    
         
     }
 
